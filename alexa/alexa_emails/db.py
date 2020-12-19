ec5662faa0ffc6f13381f17506a77ebf938f5600
aissam-out <39061689+aissam-out@users.noreply.github.com>
2020-12-19 10:17:05 +0100

    database - comments

--- alexa/alexa_emails/db.py
+++ alexa/alexa_emails/db.py
@@ -1,3 +1,4 @@
+# list of the employees' emails
 employees = {
     "aissam": "outissam92@gmail.com",
     "jack": "outissam92@gmail.com",
@@ -5,6 +6,7 @@ employees = {
     "salim": "outissam92@gmail.com"
 }
 
+# email content according to its subject
 content = {
     "remainder" : "Hello,\nThis is a remainder, please do not forget.\n\nThank You.",
     "assistance": "Hello,\nI need to see you for 5min.\n\nThank You.",
