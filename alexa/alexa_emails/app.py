2cbe5f691cc4f427ad9f4ce38703bfbd1419390f
aissam-out <39061689+aissam-out@users.noreply.github.com>
2020-12-19 10:00:46 +0100

    add app.py

---
+++ alexa/alexa_emails/app.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask_ask import Ask, statement, question, session
+import jsonify
+import requests
+import time
+import unidecode
+from alexa_emails import send_email, alexa_mail #, get_email, get_content
+
+app = Flask(__name__)
+ask = Ask(app, "/mailexa")
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@ask.launch
+def start_skill():
+    welcome_message = "Hello there, I am here to help you send emails easily .."
+    return question(welcome_message)
+
+@ask.intent('remainder', mapping={'name': 'name'})
+def send_remainder(name):
+    try:
+        answer = alexa_mail(name, 'remainder')
+    except:
+        answer = "Error !"
+    return statement(answer)
+
+
+@ask.intent('assistance', mapping={'name': 'name'})
+def send_remainder(name):
+    try:
+        answer = alexa_mail(name, 'assistance')
+    except:
+        answer = "Error !"
+    return statement(answer)
+
+@ask.intent('absence', mapping={'name': 'name'})
+def send_remainder(name):
+    try:
+        answer = alexa_mail(name, 'absence')
+    except:
+        answer = "Error !"
+    return statement(answer)
+
+if __name__ == "__main__":
+    app.run(debug=True)
