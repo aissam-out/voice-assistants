bd9d86fa2b45ebfdf7a7fa05d9cf8b82b04fa3da
aissam-out <39061689+aissam-out@users.noreply.github.com>
2020-12-19 10:14:17 +0100

    add database

---
+++ alexa/alexa_emails/db.py
@@ -0,0 +1,12 @@
+employees = {
+    "aissam": "outissam92@gmail.com",
+    "jack": "outissam92@gmail.com",
+    "youssef": "outissam92@gmail.com",
+    "salim": "outissam92@gmail.com"
+}
+
+content = {
+    "remainder" : "Hello,\nThis is a remainder, please do not forget.\n\nThank You.",
+    "assistance": "Hello,\nI need to see you for 5min.\n\nThank You.",
+    "absence": "Hello,\nI won't be able to come tomorrow.\n\nThank You."
+}
